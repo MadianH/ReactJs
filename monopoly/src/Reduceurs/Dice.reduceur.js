622340829e8b183e68a07f4813169bf99b5da055
MadianH <madianh33@gmail.com>
2019-02-07 10:29:31 +0100

    suppresion du monopoly

--- monopoly/src/Reduceurs/Dice.reduceur.js
+++
@@ -1,9 +0,0 @@
-export default function(dice = 0, action) {
-  if(action.type === 'throwing') {
-    let newDice;
-    newDice = action.result
-    return newDice
-  } else{
-    return dice
-  }
-}
