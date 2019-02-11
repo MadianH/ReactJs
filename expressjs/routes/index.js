20ddd576d4604fb33aa0d3284193e1401356e986
MadianH <madianh33@gmail.com>
2019-02-11 20:15:27 +0100

    [FEAT](index.js) : ajout de la fonction request

--- expressjs/routes/index.js
+++ expressjs/routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+// le module request permet à l'API de pouvoir request une autre API
+var request = require('request');
+
+// On va utiliser le module request, qui nécessite en premier l'URL de l'API,
+// suivi d'une fonction de callback.
+request("https://jsonplaceholder.typicode.com/users", function(error, response, body) {
+  let body;
+  body = JSON.parse(body);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
